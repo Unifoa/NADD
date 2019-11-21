9915667ec7b5d4666c19a0638c4d87d1e8f17a3b
pedro-lisboa <51640519+pedro-lisboa@users.noreply.github.com>
2019-11-21 09:12:15 -0300

    Add files via upload

---
+++ NADD Arquivos/Create Trigger TR_QUESTAO.sql
@@ -0,0 +1,17 @@
+﻿CREATE TRIGGER TR_QUESTAO
+ON Prova
+FOR INSERT
+AS
+BEGIN
+    DECLARE
+    @ID		INTEGER,
+    @QTD	INTEGER,
+	@i		INTEGER
+ 
+    SELECT @ID = ID, @QTD = Prova_Qnt_Questao, @i = 0 FROM INSERTED
+	WHILE (@i<@QTD) BEGIN
+	    SET @i=@i+1;
+		INSERT INTO AVALIACAO VALUES (@i,'-1','-1','-1',NULL,'-1','-1','-1',@ID);
+	END 
+
+END;
